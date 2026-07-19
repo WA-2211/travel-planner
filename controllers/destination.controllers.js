94c044a595042d668e68253ee898dbcb01d4aece
WA-2211 <walaaalghanmi@gmail.com>
2026-07-19 11:03:17 +0300

    fix: create destination functionality

--- controllers/destination.controllers.js
+++ controllers/destination.controllers.js
@@ -3,9 +3,10 @@ const Destination = require('../models/Destination')
 const Trip = require('../models/Trip')
 const isSignedIn = require('../middleware/is-signed-in')
 
-router.get('/new', isSignedIn, (req, res)=>{
+router.get('/new', isSignedIn, async (req, res)=>{
     try{
-        res.render('destination/create-destination.ejs')
+        const foundAllTrips = await Trip.find()
+        res.render('destination/create-destination.ejs', {trips: foundAllTrips})
     }catch(err){
         console.log('ERROR:', err)
     }
@@ -15,10 +16,20 @@ router.post('/new', isSignedIn, async (req,res)=>{
     try{
         const newDestination = await Destination.create({
             country: req.body.country,
-            city: req.body.city
+            city: req.body.city,
+            trip: req.body.trip
         })
 
         res.redirect('/destination')
+    }catch(err){
+        console.log('ERROR:', err)
+    }
+})
+
+router.get('/', isSignedIn, async (req, res)=>{
+    try{
+        const foundAllDestinations = await Destination.find().populate('trip')
+        res.render('destination/all-destinations.ejs', {destinations: foundAllDestinations})
     }catch(err){
         console.log('ERROR:', err)
     }
